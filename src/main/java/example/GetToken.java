097c13ce57378c975f8c0ca7c470bb25e1cefc36
alexk <174185310+alexkim09-max@users.noreply.github.com>
2025-10-06 14:33:50 -0400

    finished lab 5 part1-4

--- src/main/java/example/GetToken.java
+++ src/main/java/example/GetToken.java
@@ -1,5 +1,7 @@
 package example;
 
+import java.io.IOException;
+
 /**
  * Class for getting your Grade API token.
  */
@@ -13,9 +15,13 @@ public class GetToken {
         GetExample example = new GetExample();
 
         String gradeAPISignUpURL = "https://grade-apis.panchen.ca/signUp";
-        // TODO Task 1: use the run method to get your Grade API token (or obtain your token through a web browser).
-        String response = "";
-        // response = example.run( ...)
+        String response;
+        try{
+            response = example.run(gradeAPISignUpURL, "username", "CaptainCaprica");
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
         System.out.println(response);
     }
 }
